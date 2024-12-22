25b97be8fccfccb23e8e60a9c972abb99538c7fa
Syed Shahriyar Ali <shahriyarali08@gmail.com>
2024-12-22 12:52:00 +0500

    Update CORS, migrations, namespaces, and UI cleanup
    - Added CORS policy in `Program.cs` to allow requests from `https://localhost:7022`.
    - Updated `Program.cs` to apply pending database migrations on startup.
    - Corrected namespace from `BlazorWorkshop.App` to `BlazorWorkshop.APP` in `Program.cs` and `_Imports.razor`.
    - Removed redundant "Fetch data" link in `NavMenu.razor`.

--- BlazorWorkshop.API/Program.cs
+++ BlazorWorkshop.API/Program.cs
@@ -2,11 +2,23 @@ using BlazorWorkshop.API.Data;
 using BlazorWorkshop.API.Services.StudentService;
 using Microsoft.EntityFrameworkCore;
 
+var allowSpecificOrigins = "_allowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Defining CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(allowSpecificOrigins,
+        b =>
+        {
+            b.WithOrigins("https://localhost:7022").AllowAnyHeader().AllowAnyMethod()
+                .SetIsOriginAllowedToAllowWildcardSubdomains();
+        });
+});
 
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,6 +34,14 @@ builder.Services.AddScoped<IStudentService, StudentService>();
 
 var app = builder.Build();
 
+
+// Update the DB
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DataContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -30,6 +50,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(allowSpecificOrigins);
 
 app.UseAuthorization();
 
--- BlazorWorkshop.APP/Program.cs
+++ BlazorWorkshop.APP/Program.cs
@@ -1,4 +1,4 @@
-using BlazorWorkshop.App;
+using BlazorWorkshop.APP;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
